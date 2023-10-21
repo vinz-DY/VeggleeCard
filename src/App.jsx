0a02a17649da0ca04507bd44e95acb1a324f50e9
vinz-DY <vincentducamp259@hotmail.com>
2023-10-21 16:34:53 +0200

    style css dans app directement- flex contenair et color h1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
 import { useState } from 'react'
 import './App.css'
-import card from './components/card'
+import Card from './components/card'
+import styled from 'styled-components'
+import StyledComponents from './components/card'
+
+const ContainerCards = styled.div`
+display:flex;
+flex-direction: column;
+border: solid 1px black;
+`;
+
+const Title = styled.h1`
+color: orange;
+`;
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
-  )
-}
+   <ContainerCards className="containerCard">
+      <div className="headerCard">
+        <img src="src/assets\pictures/vegglee.png" alt="image" />
+      </div>
+      <div className="mainCard">
+        <Title>Vegglee</Title>
+        <h2>secondary text</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur,
+          quasi, aut autem eos quas exercitationem numquam explicabo earum
+          molestias, rem possimus maxime? Tenetur, omnis odio expedita doloribus
+          explicabo veniam minus.
+              </p>
+              <button></button>
+          </div>
+          <div className="footerCard">
+              <h2>Subtitle</h2>
+              <button>item1</button>
+              <button>item2</button>
+              <button>item3</button>
+              <button>item4</button>
+          </div>
+    </ContainerCards>
+  );
+};
 
 export default App
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+``
 
 
-
-
-const card = () => {
+const Card = () => {
   return (
-    <div>card</div>
-  )
-}
+    <div className="containerCard">
+      <div className="headerCard">
+        <img src="src/assets\pictures/vegglee.png" alt="image" />
+      </div>
+      <div className="mainCard">
+        <h1>Vegglee</h1>
+        <h2>secondary text</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur,
+          quasi, aut autem eos quas exercitationem numquam explicabo earum
+          molestias, rem possimus maxime? Tenetur, omnis odio expedita doloribus
+          explicabo veniam minus.
+              </p>
+              <button></button>
+          </div>
+          <div className="footerCard">
+              <h2>Subtitle</h2>
+              <button>item1</button>
+              <button>item2</button>
+              <button>item3</button>
+              <button>item4</button>
+          </div>
+    </div>
+  );
+};
 
-export default card
+
+const StyledComponents = () => (
+  <ContainerCards>
+    <Title>Example Styled-Components</Title>
+  </ContainerCards>
+);
+export default StyledComponents;
